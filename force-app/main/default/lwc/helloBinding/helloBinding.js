8cff5add74d767e116d5553f70fd4ac6d188d72b
akshaygawade <akshaygawade73@gmail.com>
2020-01-07 15:15:06 +0530

    test commit

--- force-app/main/default/lwc/helloBinding/helloBinding.js
+++ force-app/main/default/lwc/helloBinding/helloBinding.js
@@ -1,7 +1,8 @@
 import { LightningElement, track } from 'lwc';
 
 export default class HelloBinding extends LightningElement {
-    @track greeting = 'World';
+    @track greeting = 'Akshay';
+    @track abc = 'abc';
 
     handleChange(event) {
         this.greeting = event.target.value;
